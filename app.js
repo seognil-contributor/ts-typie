eddeb9c001e0b7a4c4273609d4bc1804c7eb1f61
seognil LC <seognil@gmail.com>
2020-04-14 16:48:10 +0800

    refactor: apply a lot of features

--- app.js
+++ app.js
@@ -1,94 +1,148 @@
 #!/usr/bin/env node
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var path = require("path");
-var child_process_1 = require("child_process");
-var request = require("request");
-var chalk = require("chalk");
-var figures = require("figures");
-var args = require("args");
-var commandExists = require("command-exists");
-// list of supported package manager tools
-// the first one found will be default
-var tools = {
-    yarn: { command: 'yarn add -D' },
-    npm: { command: 'npm install -D' }
-};
-// look for the first available tool
-var defaultTool;
-for (var _i = 0, _a = Object.keys(tools); _i < _a.length; _i++) {
-    var tool_1 = _a[_i];
-    if (commandExists.sync(tool_1)) {
-        defaultTool = tool_1;
-        break;
+const args_1 = __importDefault(require("args"));
+const chalk_1 = __importDefault(require("chalk"));
+const command_exists_1 = __importDefault(require("command-exists"));
+const child_process_1 = require("child_process");
+const figures_1 = __importDefault(require("figures"));
+const pkg_dir_1 = __importDefault(require("pkg-dir"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const registry_url_1 = __importDefault(require("registry-url"));
+const request_1 = __importDefault(require("request"));
+const ora_1 = __importDefault(require("ora"));
+(async () => {
+    // * ---------------------------------------------------------------- util
+    const b = (s) => chalk_1.default.bold(s);
+    const isTyped = (dep) => /^@types\//.test(dep);
+    const joinDeps = (deps) => deps.map((dep) => `@types/${dep}`).join(' ');
+    // * ---------------------------------------------------------------- env and checking
+    // * ---------------- tools
+    // * list of supported package manager tools
+    // * the first one found will be default
+    const tools = {
+        yarn: { install: `yarn add -D`, uninstall: `yarn remove` },
+        npm: { install: `npm install -D`, uninstall: `npm uninstall` },
+    };
+    // * ---------------- look for the first available tool
+    const defaultTool = Object.keys(tools).find((tool) => command_exists_1.default.sync(tool));
+    if (defaultTool === undefined) {
+        console.error("Couldn't find a supported package manager tool.");
     }
-}
-if (defaultTool === undefined) {
-    console.error('Couldn\'t find a supported package manager tool.');
-}
-// support for overriding default
-args.option('tool', 'Which package manager tool to use', defaultTool);
-var opts = args.parse(process.argv, {
-    name: 'ts-typie',
-    mri: undefined,
-    mainColor: 'yellow',
-    subColor: 'dim'
-});
-var tool = tools[opts.tool];
-// check if package.json exists
-var cwd = process.cwd();
-var packagePath = path.join(cwd, 'package.json');
-if (!fs.existsSync(packagePath)) {
-    console.error('No package.json file found!');
-    process.exit();
-}
-// Package.json exists
-var pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-var dependencies = [];
-if (pkg.dependencies) {
-    dependencies.push.apply(dependencies, Object.keys(pkg.dependencies));
-}
-if (pkg.devDependencies) {
-    dependencies.push.apply(dependencies, Object.keys(pkg.devDependencies));
-}
-// Filter out already installed types
-var alreadyInstalledTypes = dependencies.filter(function (d) { return /^@types\//.test(d); });
-;
-dependencies = dependencies.filter(function (d) { return !/^@types\//.test(d); });
-var _loop_1 = function (dependency) {
-    var dependencyString = chalk.bold(dependency);
-    // Check if types are already installed
-    if (alreadyInstalledTypes.includes('@types/' + dependency)) {
-        console.log(chalk.yellow(figures.play, "Types for " + dependencyString + " already installed. Skipping..."));
-        return "continue";
+    // * ---------------- support for overriding default
+    args_1.default.option('tool', 'Which package manager tool to use', defaultTool);
+    const opts = args_1.default.parse(process.argv, {
+        name: 'ts-typie',
+        mri: {},
+        mainColor: 'yellow',
+        subColor: 'dim',
+    });
+    const tool = tools[opts.tool];
+    // * ---------------- check if package.json exists
+    const cwd = process.cwd();
+    const rootDir = await pkg_dir_1.default(cwd);
+    if (rootDir === undefined) {
+        console.error('No package.json file found!');
+        process.exit();
     }
-    // Check for included types
-    var pkgPath = path.join(cwd, 'node_modules', dependency, 'package.json');
-    if (fs.existsSync(pkgPath)) {
-        var pkg_1 = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-        if (pkg_1.types || pkg_1.typings) {
-            console.log(chalk.yellow(figures.warning, "Module " + dependencyString + " includes own types. Skipping..."));
-            return "continue";
+    const packagePath = path_1.default.join(rootDir, 'package.json');
+    // * ---------------------------------------------------------------- calculation
+    const pkg = JSON.parse(fs_1.default.readFileSync(packagePath, 'utf8'));
+    const { dependencies: deps = {}, devDependencies: devDeps = {} } = pkg;
+    const allDeps = [...Object.keys(deps), ...Object.keys(devDeps), 'node'];
+    const allTypes = allDeps.filter((dep) => isTyped(dep));
+    const allNormalDeps = allDeps.filter((dep) => !isTyped(dep));
+    // * ---------------- deps crossing
+    const alreadyTypedDeps = allNormalDeps.filter((dep) => allTypes.includes(`@types/${dep}`));
+    const selfTypedDeps = allNormalDeps.filter((dep) => {
+        const pkgPath = path_1.default.join(rootDir, 'node_modules', dep, 'package.json');
+        if (fs_1.default.existsSync(pkgPath)) {
+            const pkg = JSON.parse(fs_1.default.readFileSync(pkgPath, 'utf8'));
+            if (pkg.types || pkg.typings) {
+                return true;
+            }
+            if (pkg.files && pkg.files.find((e) => /\.d\.ts/.test(e))) {
+                return true;
+            }
         }
-    }
-    // Check if types are available    
-    (function (dependency) {
-        request('https://www.npmjs.com/package/@types/' + dependency, function (err, res, body) {
-            if (res.statusCode == 200) {
-                child_process_1.exec(tool.command + " @types/" + dependency, function (err, stdout, stderr) {
-                    if (!err) {
-                        console.log(chalk.green(figures.tick, "@types/" + dependencyString + " installed successfully!"));
-                    }
-                });
-            }
-            else {
-                console.log(chalk.red(figures.cross, "No types found for " + dependencyString + " in registry. Skipping..."));
-            }
+        return false;
+    });
+    const shouldAddTypeDeps = allNormalDeps
+        .filter((dep) => !selfTypedDeps.includes(dep))
+        .filter((dep) => !alreadyTypedDeps.includes(dep));
+    // * installed, but deprecated
+    // * e.g. https://www.npmjs.com/package/@types/chalk
+    const deprecated = alreadyTypedDeps.filter((dep) => selfTypedDeps.includes(dep));
+    const unused = allTypes
+        .map((dep) => dep.replace(`@types/`, ''))
+        .filter((intalled) => !allNormalDeps.find((dep) => dep === intalled));
+    const shouldRemoveTypeDeps = [...new Set([...deprecated, ...unused])];
+    // * ---------------------------------------------------------------- display skipping
+    alreadyTypedDeps.forEach((dep) => {
+        console.log(chalk_1.default.yellow(figures_1.default.play, `Types for ${b(dep)} already installed. Skipping...`));
+    });
+    selfTypedDeps.forEach((dep) => {
+        console.log(chalk_1.default.yellow(figures_1.default.warning, `Module ${b(dep)} includes own types. Skipping...`));
+    });
+    // * ---------------------------------------------------------------- display uninstalling
+    deprecated.forEach((dep) => {
+        console.log(chalk_1.default.red(figures_1.default.cross, `@types/${b(dep)} is deprecated. Uninstalling...`));
+    });
+    unused.forEach((dep) => {
+        console.log(chalk_1.default.red(figures_1.default.cross, `@types/${b(dep)} is unused. Uninstalling...`));
+    });
+    // * ---------------------------------------------------------------- fetching
+    // `https://www.npmjs.com/@types/${dep}`
+    // `https://registry.npm.taobao.org/@types/${dep}`
+    const globalRegistry = registry_url_1.default();
+    const fetchPkg = (pkg) => {
+        // * support custom registry from config for good reason
+        // TODO mayby failure fallback ?
+        const url = `${globalRegistry}/@types/${pkg}`.replace('//@types', '/@types');
+        return new Promise((resolve) => {
+            request_1.default(url, (err, res, body) => {
+                resolve([pkg, res.statusCode === 200]);
+            });
         });
-    })(dependency);
-};
-for (var _b = 0, dependencies_1 = dependencies; _b < dependencies_1.length; _b++) {
-    var dependency = dependencies_1[_b];
-    _loop_1(dependency);
-}
+    };
+    // * parallel
+    const fetchAll = await Promise.all(shouldAddTypeDeps.map((dep) => fetchPkg(dep)));
+    const founds = fetchAll.filter(([, found]) => found).map(([dep]) => dep);
+    const notFounds = fetchAll.filter(([, found]) => !found).map(([dep]) => dep);
+    // * ---------------------------------------------------------------- display skipping, installing
+    notFounds.forEach((dep) => {
+        console.log(chalk_1.default.red(figures_1.default.cross, `No types found for ${b(dep)} in registry. Skipping...`));
+    });
+    if (founds.length) {
+        founds.forEach((dep) => {
+            console.log(chalk_1.default.green(figures_1.default.tick, `Installing @types/${b(dep)}`));
+        });
+    }
+    else {
+        console.log(chalk_1.default.white(figures_1.default.circle, `Nothing needs to be install`));
+    }
+    // * ---------------------------------------------------------------- calculate commands
+    let commands = [];
+    if (shouldRemoveTypeDeps.length) {
+        commands.push(`${tool.uninstall} ${joinDeps(shouldRemoveTypeDeps)}`);
+    }
+    if (founds.length) {
+        commands.push(`${tool.uninstall} ${joinDeps(shouldRemoveTypeDeps)}`);
+    }
+    // * ---------------------------------------------------------------- run commands or not
+    const spinner = ora_1.default('Processing...').start();
+    const complete = () => {
+        spinner.stop();
+        console.log(chalk_1.default.green(figures_1.default.tick, `Accomplished!`));
+    };
+    if (commands.length) {
+        child_process_1.exec(commands.join('; '), () => complete());
+    }
+    else {
+        complete();
+    }
+})();
--- app.ts
+++ app.ts
@@ -1,110 +1,197 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
-import * as path from 'path';
+
+import args from 'args';
+import chalk from 'chalk';
+import commandExists from 'command-exists';
 import { exec } from 'child_process';
-import * as request from 'request';
-import * as chalk from 'chalk';
-import * as figures from 'figures';
-import * as args from 'args';
-import * as commandExists from 'command-exists';
+import figures from 'figures';
+import pkgDir from 'pkg-dir';
+import fs from 'fs';
+import path from 'path';
+import registryUrl from 'registry-url';
+import request from 'request';
+import ora from 'ora';
 
+(async () => {
+  // * ---------------------------------------------------------------- util
 
-// list of supported package manager tools
-// the first one found will be default
-const tools = {
-    yarn: { command: 'yarn add -D' },
-    npm: { command: 'npm install -D' }
-};
+  const b = (s: string) => chalk.bold(s);
 
-// look for the first available tool
-let defaultTool;
-for (const tool of Object.keys(tools)) {
-    if (commandExists.sync(tool)) {
-        defaultTool = tool;
-        break;
-    }
-}
-if (defaultTool === undefined) {
-    console.error('Couldn\'t find a supported package manager tool.')
-}
+  const isTyped = (dep: string) => /^@types\//.test(dep);
 
-// support for overriding default
-args.option('tool', 'Which package manager tool to use', defaultTool);
-const opts = args.parse(process.argv, {
+  const joinDeps = (deps: string[]) => deps.map((dep) => `@types/${dep}`).join(' ');
+
+  // * ---------------------------------------------------------------- env and checking
+
+  // * ---------------- tools
+  // * list of supported package manager tools
+  // * the first one found will be default
+
+  const tools = {
+    yarn: { install: `yarn add -D`, uninstall: `yarn remove` },
+    npm: { install: `npm install -D`, uninstall: `npm uninstall` },
+  };
+
+  // * ---------------- look for the first available tool
+
+  const defaultTool = Object.keys(tools).find((tool) => commandExists.sync(tool));
+
+  if (defaultTool === undefined) {
+    console.error("Couldn't find a supported package manager tool.");
+  }
+
+  // * ---------------- support for overriding default
+
+  args.option('tool', 'Which package manager tool to use', defaultTool);
+  const opts = args.parse(process.argv, {
     name: 'ts-typie',
-    mri: undefined,
+    mri: {},
     mainColor: 'yellow',
-    subColor: 'dim'
-});
-const tool = tools[opts.tool];
+    subColor: 'dim',
+  });
+  const tool = tools[opts.tool as keyof typeof tools];
 
+  // * ---------------- check if package.json exists
 
+  const cwd = process.cwd();
+  const rootDir = await pkgDir(cwd);
 
-// check if package.json exists
-
-const cwd = process.cwd();
-const packagePath = path.join(cwd, 'package.json');
-
-if (!fs.existsSync(packagePath)) {
+  if (rootDir === undefined) {
     console.error('No package.json file found!');
     process.exit();
-}
+  }
 
-// Package.json exists
+  const packagePath = path.join(rootDir, 'package.json');
 
-const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-let dependencies: string[] = [];
+  // * ---------------------------------------------------------------- calculation
 
-if (pkg.dependencies) {
-    dependencies.push(...Object.keys(pkg.dependencies));
-}
-if (pkg.devDependencies) {
-    dependencies.push(...Object.keys(pkg.devDependencies));
-}
+  const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
-// Filter out already installed types
+  const { dependencies: deps = {}, devDependencies: devDeps = {} } = pkg;
 
-let alreadyInstalledTypes = dependencies.filter(d => /^@types\//.test(d));;
-dependencies = dependencies.filter(d => !/^@types\//.test(d));
+  const allDeps = [...Object.keys(deps), ...Object.keys(devDeps), 'node'];
 
-for (let dependency of dependencies) {
-    const dependencyString = chalk.bold(dependency)
+  const allTypes = allDeps.filter((dep) => isTyped(dep));
+  const allNormalDeps = allDeps.filter((dep) => !isTyped(dep));
 
-    // Check if types are already installed
+  // * ---------------- deps crossing
 
-    if (alreadyInstalledTypes.includes('@types/' + dependency)) {
-        console.log(chalk.yellow(figures.play, `Types for ${dependencyString} already installed. Skipping...`));
-        continue;
-    }
+  const alreadyTypedDeps = allNormalDeps.filter((dep) => allTypes.includes(`@types/${dep}`));
 
-    // Check for included types
-    let pkgPath = path.join(cwd, 'node_modules', dependency, 'package.json');
-
+  const selfTypedDeps = allNormalDeps.filter((dep) => {
+    const pkgPath = path.join(rootDir, 'node_modules', dep, 'package.json');
 
     if (fs.existsSync(pkgPath)) {
-        const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-        if (pkg.types || pkg.typings) {
-            console.log(chalk.yellow(figures.warning, `Module ${dependencyString} includes own types. Skipping...`));
-            continue;
-        }
+      const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+
+      if (pkg.types || pkg.typings) {
+        return true;
+      }
+
+      if (pkg.files && (pkg.files as string[]).find((e) => /\.d\.ts/.test(e))) {
+        return true;
+      }
     }
 
-    // Check if types are available    
+    return false;
+  });
 
-    ((dependency) => {
-        request('https://www.npmjs.com/package/@types/' + dependency, (err, res, body) => {
+  const shouldAddTypeDeps = allNormalDeps
+    .filter((dep) => !selfTypedDeps.includes(dep))
+    .filter((dep) => !alreadyTypedDeps.includes(dep));
 
-            if (res.statusCode == 200) {
-                exec(`${tool.command} @types/${dependency}`, (err, stdout, stderr) => {
-                    if (!err) {
-                        console.log(chalk.green(figures.tick, `@types/${dependencyString} installed successfully!`));
-                    }
-                });
-            } else {
-                console.log(chalk.red(figures.cross, `No types found for ${dependencyString} in registry. Skipping...`));
-            }
+  // * installed, but deprecated
+  // * e.g. https://www.npmjs.com/package/@types/chalk
+  const deprecated = alreadyTypedDeps.filter((dep) => selfTypedDeps.includes(dep));
 
-        });
-    })(dependency);
+  const unused = allTypes
+    .map((dep: string) => dep.replace(`@types/`, ''))
+    .filter((intalled) => !allNormalDeps.find((dep) => dep === intalled));
 
-}
+  const shouldRemoveTypeDeps = [...new Set([...deprecated, ...unused])];
+
+  // * ---------------------------------------------------------------- display skipping
+
+  alreadyTypedDeps.forEach((dep) => {
+    console.log(chalk.yellow(figures.play, `Types for ${b(dep)} already installed. Skipping...`));
+  });
+
+  selfTypedDeps.forEach((dep) => {
+    console.log(chalk.yellow(figures.warning, `Module ${b(dep)} includes own types. Skipping...`));
+  });
+
+  // * ---------------------------------------------------------------- display uninstalling
+
+  deprecated.forEach((dep) => {
+    console.log(chalk.red(figures.cross, `@types/${b(dep)} is deprecated. Uninstalling...`));
+  });
+
+  unused.forEach((dep) => {
+    console.log(chalk.red(figures.cross, `@types/${b(dep)} is unused. Uninstalling...`));
+  });
+
+  // * ---------------------------------------------------------------- fetching
+
+  // `https://www.npmjs.com/@types/${dep}`
+  // `https://registry.npm.taobao.org/@types/${dep}`
+
+  const globalRegistry = registryUrl();
+  const fetchPkg = (pkg: string) => {
+    // * support custom registry from config for good reason
+    // TODO mayby failure fallback ?
+    const url = `${globalRegistry}/@types/${pkg}`.replace('//@types', '/@types');
+
+    return new Promise<[string, boolean]>((resolve) => {
+      request(url, (err, res, body) => {
+        resolve([pkg, res.statusCode === 200]);
+      });
+    });
+  };
+
+  // * parallel
+  const fetchAll = await Promise.all(shouldAddTypeDeps.map((dep) => fetchPkg(dep)));
+
+  const founds = fetchAll.filter(([, found]) => found).map(([dep]) => dep);
+  const notFounds = fetchAll.filter(([, found]) => !found).map(([dep]) => dep);
+
+  // * ---------------------------------------------------------------- display skipping, installing
+
+  notFounds.forEach((dep) => {
+    console.log(chalk.red(figures.cross, `No types found for ${b(dep)} in registry. Skipping...`));
+  });
+
+  if (founds.length) {
+    founds.forEach((dep) => {
+      console.log(chalk.green(figures.tick, `Installing @types/${b(dep)}`));
+    });
+  } else {
+    console.log(chalk.white(figures.circle, `Nothing needs to be install`));
+  }
+
+  // * ---------------------------------------------------------------- calculate commands
+
+  let commands: string[] = [];
+
+  if (shouldRemoveTypeDeps.length) {
+    commands.push(`${tool.uninstall} ${joinDeps(shouldRemoveTypeDeps)}`);
+  }
+
+  if (founds.length) {
+    commands.push(`${tool.uninstall} ${joinDeps(shouldRemoveTypeDeps)}`);
+  }
+
+  // * ---------------------------------------------------------------- run commands or not
+
+  const spinner = ora('Processing...').start();
+
+  const complete = () => {
+    spinner.stop();
+    console.log(chalk.green(figures.tick, `Accomplished!`));
+  };
+
+  if (commands.length) {
+    exec(commands.join('; '), () => complete());
+  } else {
+    complete();
+  }
+})();
